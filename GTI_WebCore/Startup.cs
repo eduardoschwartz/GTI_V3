e98146384c98eb4efa9876d2cddc1fb71037461e
notebook <eduardo.schwartz@gmail.com>
2019-07-31 09:14:32 -0300

    WebCore

---
+++ GTI_WebCore/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GTI_WebCore {
+    public class Startup {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.addmvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.Run(async (context) => {
+                throw new Exception("Some error processing the request");
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
